e26c279477eff873fc97990c4f8ee7c729b2666b
owoichoche <owoichoche@gmail.com>
2022-07-14 01:36:12 +0100

    new commits

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -1,5 +1,21 @@
-#This program calculates the Body Mass Index of a person and prints the result as an integer
-height = input("enter your height in m: ")
-weight = input("enter your weight in kg: ")
-bmi = (int(weight) / (float(height) ** 2))
-print(int(bmi))
+# This program calculates the Body Mass Index of a person and prints the result 
+# following the conditions in the if statements below.
+# Note here that the program takes in the height and weight as floating point numbers
+# and rounds up the result to a whole number
+
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
+result = round(weight / height ** 2)
+
+if result < 18.5:
+    result_statement = ("you are underweight.")
+elif result < 25:
+    result_statement = ("you have a normal weight.")
+elif result < 30:
+    result_statement = ("you are slightly overweight.")
+elif result < 35:
+    result_statement = ("you are obese.")
+elif result > 35:
+    result_statement = ("you are clinically obese.")
+
+print(f"Your BMI is {result}, {result_statement}")
