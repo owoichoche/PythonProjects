0e9fb9f0211d5e8e396eadf323a289ce4bb25c98
owoichoche <owoichoche@gmail.com>
2022-07-28 22:00:46 +0100

    Initial commit
    Simple use case for nested lists

---
+++ nestedlists_intro.py
@@ -0,0 +1,9 @@
+# This program plays around nested lists. 
+# Have fun!
+
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+
+dirty_dozen = [fruits, vegetables]
+
+print(dirty_dozen[1][1])
